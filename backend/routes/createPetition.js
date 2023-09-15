5d6fca171d3dcb65df0ebdde8b61e334bfbd6319
jenn1705 <99060936+jentaruno@users.noreply.github.com>
2023-09-15 13:28:02 -0700

    get petition works

---
+++ backend/routes/createPetition.js
@@ -0,0 +1,66 @@
+const {connectDatabase} = require("../utils");
+const {Petitions} = require("../models/petition");
+const {Users} = require("../models/user");
+const mongoose = require("mongoose");
+const express = require("express");
+const router = express.Router()
+
+// Create a petition
+// Takes petition schema and user ID
+// Add petition to DB
+// Add petition to user's created petitions
+// Request body example:
+/*
+    {
+        user: "650494d18c60798c602ffbba",
+        petition: {
+                title: "title",
+                company: "company",
+                location: "loc",
+                poster: "650494d18c60798c602ffbba",
+            }
+    }
+ */
+router.post('', async (req, res) => {
+    await connectDatabase(res)
+    try {
+        console.log("trying")
+        const { userId, petition } = req.body;
+            let newPetition = await Petitions.create(petition);
+            let petitionId = newPetition._id;
+            await addPetitionToUser(userId, petitionId);
+        res.status(200).send("Created Petition " + petition.title);
+    } catch (error) {
+        console.error(error);
+        res.status(400).send("Couldn't create petition")
+
+    } finally {
+        mongoose.connection.close()
+    }
+});
+
+async function addPetitionToUser(userId, petitionId) {
+    try {
+        const user = await Users.findById(userId);
+        if (!user) {
+            console.error('User not found');
+            return;
+        }
+
+        const petition = await Petitions.findById(petitionId);
+        if (!petition) {
+            console.error('Petition not found');
+            return;
+        }
+
+        user.petitions.created.push(petition);
+
+        await user.save();
+
+        console.log(`Added petition to user's created petitions: User ID ${userId}, Petition ID ${petitionId}`);
+    } catch (error) {
+        console.error('Error:', error);
+    }
+}
+
+module.exports = router
--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const cors = require('cors');
 const initRouter = require('./routes/init');
+const createPetitionRouter = require('./routes/createPetition')
+const getPetitionRouter = require('./routes/getPetition')
 
 const app = express();
 app.use(cors());
 
 app.use('/init', initRouter);
+app.use('/petitions/create', createPetitionRouter);
+app.use('/petitions', getPetitionRouter);
 
 app.listen(8080, () => console.log('API is running on http://localhost:8080'));
