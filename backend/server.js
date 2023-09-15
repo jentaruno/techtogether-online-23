a7438bf7c9b539c98e8ff9862aeada05eb200166
abcd <jputna01@students.ubc.ca>
2023-09-15 15:47:55 -0700

    Added newUser, editUser, getUser functionality, to be tested

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const cors = require('cors');
 const initRouter = require('./routes/init');
+//const newUserRouter = require('./routes/newUser');
 
 const app = express();
 app.use(cors());
+app.use(express.json());
 
 app.use('/init', initRouter);
 
