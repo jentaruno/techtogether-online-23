114cfee1949dd80c596075f16eec0963cc2392f0
jenn1705 <99060936+jentaruno@users.noreply.github.com>
2023-09-17 01:23:40 -0700

    Load all petitions

--- frontend/app/petition/page.jsx
+++ frontend/app/petition/page.jsx
@@ -1,69 +1,24 @@
-import React from "react";
+"use client"
+import React, {useEffect, useState} from "react";
 import Navbar from "@components/Navbar";
-import Petition from "@components/Petition";
-import {Box, Grid} from "@mui/material";
+import PetitionGrid from "@components/PetitionGrid";
+import {getAllPetitions} from "@data/petitions";
 
 export default function Page() {
+    const [petitions, setPetitions] = useState([]);
+    useEffect(() => {
+        async function fetchData() {
+            setPetitions(await getAllPetitions());
+        }
 
-    const petitionsArray = [
-        {
-            title: "Title",
-            company: "Company",
-            location: "Location",
-            description: "Description ..."
-        },
-        {
-            title: "Title2",
-            company: "Company22",
-            location: "Location2",
-            description: "Description esufhouwhf ..."
-        },
-        {
-            title: "Title",
-            company: "Company",
-            location: "Location",
-            description: "Description ..."
-        },
-        {
-            title: "Title2",
-            company: "Company22",
-            location: "Location2",
-            description: "Description esufhouwhf ..."
-        },
-        {
-            title: "Title",
-            company: "Company",
-            location: "Location",
-            description: "Description ..."
-        },
-        {
-            title: "Title2",
-            company: "Company22",
-            location: "Location2",
-            description: "Description esufhouwhf ..."
-        },
-    ];
+        fetchData();
+    }, [])
 
     return (
         <div className="flex flex-col w-full items-center">
             <Navbar/>
             <div className={"w-2/3 mt-12"}>
-                <Grid container spacing={2}>
-                    {petitionsArray.map(e =>
-                        <Grid item xs={4}>
-                            <Box p={2} sx={{boxShadow: 1}}>
-                                <Petition
-                                    petition={{
-                                        title: e.title,
-                                        company: e.company,
-                                        location: e.location,
-                                        description: e.description
-                                    }}
-                                />
-                            </Box>
-                        </Grid>
-                    )}
-                </Grid>
+                <PetitionGrid petitionsArray={petitions}/>
             </div>
         </div>
     );
---
+++ frontend/components/PetitionGrid.jsx
@@ -0,0 +1,20 @@
+import {Box, Grid} from "@node_modules/@mui/material";
+import Petition from "@components/Petition";
+import React from "react";
+
+export default function PetitionGrid({petitionsArray}) {
+    return <Grid container spacing={2}>
+        {petitionsArray.map(e => <Grid item xs={4}>
+            <Box p={2} sx={{boxShadow: 1}}>
+                <Petition
+                    petition={{
+                        title: e.title,
+                        company: e.company,
+                        location: e.location,
+                        description: e.description
+                    }}
+                />
+            </Box>
+        </Grid>)}
+    </Grid>;
+}
