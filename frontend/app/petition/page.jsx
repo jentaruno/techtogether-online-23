9067df11b19e973d185f9dde7f14fd2f5b4cab18
jenn1705 <99060936+jentaruno@users.noreply.github.com>
2023-09-17 00:52:14 -0700

    All petitions page update

--- frontend/app/petition/page.jsx
+++ frontend/app/petition/page.jsx
@@ -1,23 +1,70 @@
 import React from "react";
 import Navbar from "@components/Navbar";
 import Petition from "@components/Petition";
-import {Box} from "@mui/material";
+import {Box, Grid} from "@mui/material";
 
 export default function Page() {
 
+    const petitionsArray = [
+        {
+            title: "Title",
+            company: "Company",
+            location: "Location",
+            description: "Description ..."
+        },
+        {
+            title: "Title2",
+            company: "Company22",
+            location: "Location2",
+            description: "Description esufhouwhf ..."
+        },
+        {
+            title: "Title",
+            company: "Company",
+            location: "Location",
+            description: "Description ..."
+        },
+        {
+            title: "Title2",
+            company: "Company22",
+            location: "Location2",
+            description: "Description esufhouwhf ..."
+        },
+        {
+            title: "Title",
+            company: "Company",
+            location: "Location",
+            description: "Description ..."
+        },
+        {
+            title: "Title2",
+            company: "Company22",
+            location: "Location2",
+            description: "Description esufhouwhf ..."
+        },
+    ];
+
     return (
-        <main className="w-full h-screen ">
+        <div className="flex flex-col w-full items-center">
             <Navbar/>
-            <Box p={2} sx={{boxShadow: 1, width: '20rem'}}>
-                <Petition
-                    petition={{
-                        title: "Title",
-                        company: "Company",
-                        location: "Location",
-                        description: "Description ..."
-                    }}
-                />
-            </Box>
-        </main>
+            <div className={"w-2/3 mt-12"}>
+                <Grid container spacing={2}>
+                    {petitionsArray.map(e =>
+                        <Grid item xs={4}>
+                            <Box p={2} sx={{boxShadow: 1}}>
+                                <Petition
+                                    petition={{
+                                        title: e.title,
+                                        company: e.company,
+                                        location: e.location,
+                                        description: e.description
+                                    }}
+                                />
+                            </Box>
+                        </Grid>
+                    )}
+                </Grid>
+            </div>
+        </div>
     );
 }
