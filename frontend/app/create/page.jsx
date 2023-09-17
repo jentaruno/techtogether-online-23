6d770e651ff4210a81cf6b4a46ebf3b48acaedb2
Beta-test-initiation <85096070+Beta-test-initiation@users.noreply.github.com>
2023-09-16 18:24:31 -0700

    added create petition page and view petition page along with dynamic routing

--- frontend/app/create/page.jsx
+++ frontend/app/create/page.jsx
@@ -1,9 +1,101 @@
-import React from 'react'
+"use client";
 
-const page = () => {
+import Navbar from '@components/Navbar';
+import React, { useState } from 'react';
+
+const PetitionForm = () => {
+  const [formData, setFormData] = useState({
+    title: '',
+    company: '',
+    location: '',
+    description: '',
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch('/your-api-endpoint', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+      if (response.ok) {
+        console.log('Petition created successfully.');
+        // Reset the form or perform any other actions as needed.
+      } else {
+        console.error('Error creating petition.');
+      }
+    } catch (error) {
+      console.error('Error creating petition:', error);
+    }
+  };
+
   return (
-    <div>page</div>
-  )
-}
+    <div className="h-screen w-full">
+      <Navbar/>
+      <div className="h-full flex flex-col justify-center items-center bg-primary-lightblue">
+        <div className="flex flex-col items-center rounded-lg gap-20  bg-white max-w-2xl mt-60 rounded-lg mb-40">
+          <div className="text-4xl mt-16 text-primary-darkblue font-bold ">Create Your Voice</div>
+          <form onSubmit={handleSubmit}>
+            <div className="flex flex-col justify-left items-start px-24 pb-16">
+              <input
+                type="text"
+                name="title"
+                placeholder="Title"
+                value={formData.title}
+                onChange={handleChange}
+                style={{ width: '100%'}}
+                className="text-3xl font-bold border rounded-lg border-gray-300 p-2 text-primary-darkblue mb-10"
+              />
+              <input
+                type="text"
+                name="company"
+                placeholder="Company"
+                value={formData.company}
+                onChange={handleChange}
+                style={{ width: '100%'}}
+                className="text-2xl text-primary-darkblue rounded-lg border border-gray-300 p-2 mb-10"
+              />
+              <input
+                type="text"
+                name="location"
+                placeholder="Location"
+                value={formData.location}
+                onChange={handleChange}
+                style={{ width: '100%'}}
+                className="text-2xl text-primary-darkblue rounded-lg border border-gray-300 p-2 mb-10"
+              />
+              <textarea
+                name="description"
+                placeholder="Description"
+                value={formData.description}
+                onChange={handleChange}
+                className="text-sm mb-10 rounded-lg border border-gray-300 p-2 "
+                style={{ width: '100%', height: '80px' }}
+              />
+              <button
+                type="submit"
+                className="bg-primary-darkblue text-white p-3 rounded"
+              >
+                Create Petition!
+              </button>
+            </div>
+            
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default page
+export default PetitionForm;
